1767a34a06bd4df80f891efa3519812f704d28ba
WaterKing201030 <947488670@qq.com>
2024-05-29 20:05:16 +0800

    add predefined tags 添加了一些预先定义的Tag

--- tags.cpp
+++ tags.cpp
@@ -1,6 +1,39 @@
 #include"tags.h"
 
 DishTag::DishTag(TagKind _kind):kind(_kind){}
+DishNormalTag::DishNormalTag(QString & _name):DishTag(TagKind::NORMAL), name(_name){}
 DishKindTag::DishKindTag(DishKind _kind):DishTag(TagKind::KIND), dish_kind(_kind){}
 DishTasteTag::DishTasteTag(DishTaste _taste):DishTag(TagKind::TASTE), taste(_taste){}
 DishWaitTag::DishWaitTag(bool _wait):DishTag(TagKind::WAIT), should_wait(_wait){}
+
+DishKindTag DISHKINDTAG_TAOCAN = DishKindTag(DishKind::TAOCAN);
+DishKindTag DISHKINDTAG_ZHUSHI = DishKindTag(DishKind::ZHUSHI);
+DishKindTag DISHKINDTAG_SHAOKAO = DishKindTag(DishKind::SHAOKAO);
+DishKindTag DISHKINDTAG_TIANDIAN = DishKindTag(DishKind::TIANDIAN);
+DishKindTag DISHKINDTAG_ROU = DishKindTag(DishKind::ROU);
+DishKindTag DISHKINDTAG_CAI = DishKindTag(DishKind::CAI);
+
+DishTasteTag DISHTASTETAG_NOSPICY = DishTasteTag(DishTaste::NOT_SPICY);
+DishTasteTag DISHTASTETAG_LISPICY = DishTasteTag(DishTaste::LITTLE_SPICY);
+DishTasteTag DISHTASTETAG_MISPICY = DishTasteTag(DishTaste::MIDDLE_SPICY);
+DishTasteTag DISHTASTETAG_HESPICY = DishTasteTag(DishTaste::HEAVY_SPICY);
+
+DishWaitTag DISHWAITTAG_TRUE = DishWaitTag(true);
+DishWaitTag DISHWAITTAG_FALSE = DishWaitTag(false);
+
+QMap<QString, DishKindTag*> predefine_tags = {
+    {"套餐", &DISHKINDTAG_TAOCAN},
+    {"主食", &DISHKINDTAG_ZHUSHI},
+    {"烧烤", &DISHKINDTAG_SHAOKAO},
+    {"甜点", &DISHKINDTAG_TIANDIAN},
+    {"肉", &DISHKINDTAG_ROU},
+    {"素", &DISHKINDTAG_CAI},
+
+    {"不辣", &DISHTASTETAG_NOSPICY},
+    {"微辣", &DISHTASTETAG_LISPICY},
+    {"中辣", &DISHTASTETAG_MISPICY},
+    {"重辣", &DISHTASTETAG_HESPICY},
+
+    {"无需等待", &DISHWAITTAG_FALSE},
+    {"需要等待", &DISHWAITTAG_TRUE}
+};
--- tags.h
+++ tags.h
@@ -1,18 +1,25 @@
 #ifndef _TAGS_H
 #define _TAGS_H
 
-#include<Qt>
+#include<QString>
+#include<QMap>
 
 enum class TagKind{
   KIND,
   TASTE,
-  WAIT
+  WAIT,
+  NORMAL
 };
 
 struct DishTag{
   TagKind kind;
   protected:
   DishTag(TagKind _kind);
+};
+
+struct DishNormalTag:public DishTag{
+  QString name;
+  DishNormalTag(QString & _name);
 };
 
 enum class DishKind{
@@ -22,6 +29,24 @@ enum class DishKind{
   TIANDIAN,
   ROU,
   CAI
+};
+
+QMap<DishKind, QString> DISHKIND_NAME={
+    {DishKind::TAOCAN, "套餐"},
+    {DishKind::ZHUSHI, "主食"},
+    {DishKind::SHAOKAO, "烧烤"},
+    {DishKind::TIANDIAN, "甜点"},
+    {DishKind::ROU, "荤"},
+    {DishKind::CAI, "素"}
+};
+
+QMap<QString, DishKind> NAME_DISHKIND={
+    {"套餐", DishKind::TAOCAN},
+    {"主食", DishKind::ZHUSHI},
+    {"烧烤", DishKind::SHAOKAO},
+    {"甜点", DishKind::TIANDIAN},
+    {"荤", DishKind::ROU},
+    {"素", DishKind::CAI}
 };
 
 struct DishKindTag:public DishTag{
@@ -45,5 +70,7 @@ struct DishWaitTag:public DishTag{
   bool should_wait;
   DishWaitTag(bool _wait = false);
 };
+
+extern QMap<QString, DishKindTag*> predefine_tags;
 
 #endif
