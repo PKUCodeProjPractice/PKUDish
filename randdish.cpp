1bd17e6369c9130ea18d2906a1100fb00c239132
WaterKing201030 <947488670@qq.com>
2024-05-29 18:50:56 +0800

    refrac & feat filter on main, meat... 修改了针对套餐、主食等食物种类tag的筛选的写法

--- randdish.cpp
+++ randdish.cpp
@@ -1,6 +1,36 @@
+#include<optional>
 #include<random>
 #include"randdish.h"
 using namespace std;
+
+// 不在头文件中
+optional<Dish> randChoiceOneFromTag(Dishes & dishes, QString & tag, Canteen & canteen){
+    Dishes filt;
+    if(canteen != CANTEEN_UNDEF){
+        filt = dishes.filterCanteen(canteen); // 这个if或许可以通过修改过滤的实现方式合并
+    }else{
+        filt = dishes;
+    }
+    if(filt.size() <= 0) return nullopt;
+    filt = filt.searchInTags(tag);
+    if(filt.size() <= 0) return nullopt;
+    uniform_int_distribution distrib(0, filt.size());
+    Dish choice = dishes.getAllDishes()[distrib(gen)];
+    if(canteen == CANTEEN_UNDEF){
+        canteen = choice.canteen;
+    }
+    return choice;
+}
+
+// 不在头文件中
+void appendDishChoice(Dishes & res, Dishes & dishes, QString & tag, Canteen & canteen){
+    auto dish = randChoiceOneFromTag(dishes, tag, canteen);
+    if(dish.has_value()){
+        res.append(dish);
+    }else{
+        // TODO:这里可能得加个报错，用来在前端显示
+    }
+}
 
 Dishes randChoice(Dishes & dishes, const RandConfig & config){
     // 初始化随机数
@@ -14,23 +44,13 @@ Dishes randChoice(Dishes & dishes, const RandConfig & config){
     }
     // TODO：辣度
     // 需要定义辣度上的序关系
+    
     // 选择一个菜品，查看它的食堂; 用第一个菜确定食堂
-    // TODO：用更好的方式进行遍历，而不是现在这样来一个条件判断一次
     Dishes res;
     Canteen canteen = CANTEEN_UNDEF;
-    if(config.has_pack){
-        Dishes filt2;
-        if(canteen != CANTEEN_UNDEF){
-            filt2 = filt.filterCanteen(canteen); // 这个if或许可以通过修改过滤的实现方式合并
-        }else{
-            filt2 = filt;
-        }
-        uniform_int_distribution distrib(0, filt2.size());
-        Dish choice = filt.getAllDishes()[distrib(gen)];
-        if(canteen == CANTEEN_UNDEF){
-            canteen = choice.canteen;
-        }
-        res.append(choice);
-    }
+    if(config.has_pack)appendDishChoice(res, filt, "套餐", canteen);
+    if(config.has_main)appendDishChoice(res, filt, "主食", canteen);
+    if(config.has_meat)appendDishChoice(res, filt, "荤", canteen);
+    if(config.has_vege)appendDishChoice(res, filt, "素", canteen);
     return res;
 }
