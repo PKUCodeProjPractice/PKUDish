24b4572c0a4bc2d41223a02cf83efe98c726bfdc
WaterKing201030 <947488670@qq.com>
2024-05-30 19:37:57 +0800

    尝试构建随机页面的控制器

--- randomtab.cpp
+++ randomtab.cpp
@@ -1,8 +1,11 @@
 ﻿#include "randomtab.h"
 #include "ui_randomtab.h"
 
+RandomTabCtrl::RandomTabCtrl(const RandomTab* _view):view(_view){}
+
 RandomTab::RandomTab(QWidget *parent)
     : QWidget(parent)
+    , controller(this)
     , ui(new Ui::RandomTab)
 {
     ui->setupUi(this);
@@ -11,4 +14,9 @@ RandomTab::RandomTab(QWidget *parent)
 RandomTab::~RandomTab()
 {
     delete ui;
+}
+
+RandConfig RandomTab::dumpConfig(){
+    RandConfig config;
+
 }
--- randomtab.h
+++ randomtab.h
@@ -2,18 +2,29 @@
 #define RANDOMTAB_H
 
 #include <QWidget>
+#include "randdish.h"
 
 namespace Ui {
 class RandomTab;
 }
+
+class RandomTab;
+
+class RandomTabCtrl{
+    const RandomTab* view;
+public:
+    RandomTabCtrl(const RandomTab* _view);
+};
 
 class RandomTab : public QWidget
 {
     Q_OBJECT
 
 public:
+    RandomTabCtrl controller;
     explicit RandomTab(QWidget *parent = nullptr);
     ~RandomTab();
+    RandConfig dumpConfig();
 
 private:
     Ui::RandomTab *ui;
